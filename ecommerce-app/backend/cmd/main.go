0c547c6625e75e8e9718febb803bd28be4fddccd
hoanghungbk93 <hch.bkhn@gmail.com>
2025-08-27 21:39:07 +0700

    fix bub install

--- ecommerce-app/backend/cmd/main.go
+++ ecommerce-app/backend/cmd/main.go
@@ -34,7 +34,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001", "http://ecommerce.itmf.com.vn", "https://ecommerce.itmf.com.vn"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
